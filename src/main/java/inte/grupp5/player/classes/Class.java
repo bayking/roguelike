877b6afff08129495efe882035b0ef23b30d13fe
bayking <31547998+bayking@users.noreply.github.com>
2020-10-14 16:40:33 +0200

    Abstract class Class(Mage, Paladin in the future etc) added for Player. Might rename. Passes tests for current methods

---
+++ src/main/java/inte/grupp5/player/classes/Class.java
@@ -0,0 +1,35 @@
+package inte.grupp5.player.classes;
+
+public abstract class  Class {
+    private int[] healthPerLvl = new int[60];
+    private int[] manaPerLvl = new int[60];
+    // private ArrayList<Spell> spells
+
+    void initArrays(int health, int mana) {
+        if (health <= 0 || mana <= 0) {
+            throw new IllegalArgumentException();
+        }
+        for (int i = 0; i<=59;i++) {
+            healthPerLvl[i] = health;
+            manaPerLvl[i] = mana;
+            health = health+100;
+            mana = mana+100;
+        }
+    }
+
+    /*public abstract int getHealth(int level);
+    public abstract int getMana(int level);*/
+    public int getHealth(int level) {
+        if (level <= 0 || level > 60)
+            throw new IllegalArgumentException("Level must be in range 1-60");
+        return healthPerLvl[level-1];
+    }
+
+    public int getMana(int level) {
+        if (level < 0 || level > 60)
+            throw new IllegalArgumentException("Level must be in range 1-60");
+        if (level == 0)
+            return manaPerLvl[level];
+        return manaPerLvl[level-1];
+    }
+}
---
+++ src/test/java/inte/grupp5/player/classes/ClassTest.java
@@ -0,0 +1,70 @@
+package inte.grupp5.player.classes;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ClassTest {
+    Class c1 = new Class() {
+    };
+
+    @Test
+    void initArrayWithNegativeValues() {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> c1.initArrays(-1, -3));
+    }
+
+    @Test
+    void initArrayWithZeros() {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> c1.initArrays(0, 0));
+    }
+
+    @Test
+    void initArrayWithPositiveValues() {
+        int health = 100;
+        int mana = 50;
+        c1.initArrays(health,mana);
+        for (int i = 1; i<=60;i++) {
+            assertEquals(health, c1.getHealth(i));
+            assertEquals(mana, c1.getMana(i));
+            health+=100;
+            mana+=100;
+        }
+    }
+
+    @Test
+    void initArrayWithNegativeAndPositiveValues() {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> c1.initArrays(12, 0));
+    }
+
+    @Test
+    void getHealthWithZero() {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> c1.getHealth(0));
+    }
+
+    @Test
+    void getHealthWithNegative() {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> c1.getHealth(-1));
+    }
+
+    @Test
+    void getHealthWithAboveSixty() {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> c1.getHealth(61));
+    }
+
+    @Test
+    void getHealthWithOne() {
+        Class c2 = new Class() {
+        };
+        c2.initArrays(100, 100);
+        assertEquals(100, c2.getHealth(1));
+    }
+
+    @Test
+    void getHealthWithSixty() {
+        Class c2 = new Class() {
+        };
+        c2.initArrays(100, 100);
+        assertEquals(6000, c2.getHealth(60));
+    }
+}
