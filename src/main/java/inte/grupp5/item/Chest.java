de49bb024580d66d25a007ad621895b5f2d32fef
Jonathan <joas4713@student.dsv.se>
2020-10-20 15:20:58 +0200

    Merged with Player.development. Chest test and class implementation.

--- src/main/java/inte/grupp5/item/Chest.java
+++ src/main/java/inte/grupp5/item/Chest.java
@@ -1,6 +1,7 @@
 package inte.grupp5.item;
 
 import inte.grupp5.player.Player;
+import inte.grupp5.player.classes.Mage;
 
 import java.util.ArrayList;
 
@@ -8,8 +9,9 @@ public class Chest {
 
     private ArrayList<Item> items = new ArrayList<>();
 
-    // TODO: Implement for all the Player Classes and Player levels.
+    // TODO: Implement for all the Player Classes.
     public void generateItems(Player player) {
+        int WEAPON_DMG_LVL_1 = 5;
         int WEAPON_DMG_LVL_10 = 10;
         int WEAPON_DMG_LVL_20 = 20;
         int WEAPON_DMG_LVL_30 = 30;
@@ -33,7 +35,7 @@ public class Chest {
         items.add(new Potion(HEALTH_POTION, 2, Potion.PotionType.HEALTH_POTION));
         items.add(new Potion(LEVEL_POTION, 1, Potion.PotionType.LEVEL_POTION));
 
-        if (player instanceof Mage) {
+        if (player.getKlass() instanceof Mage) {
 
             // Every chest has a mana potion when you are a mage.
             items.add(new Potion(MANA_POTION, 2, Potion.PotionType.MANA_POTION));
@@ -41,20 +43,23 @@ public class Chest {
             // Based on player level, adds Staff with different damage.
             if (player.getLevel() <= 10) {
                 items.add(new Weapon
-                        (STAFF, STAFF_WEIGHT, WEAPON_DMG_LVL_10, Weapon.WeaponType.STAFF));
+                        (STAFF, STAFF_WEIGHT, WEAPON_DMG_LVL_1, Weapon.WeaponType.STAFF));
             } else if (player.getLevel() > 10 && player.getLevel() <= 20) {
                 items.add(new Weapon
-                        (STAFF, STAFF_WEIGHT, WEAPON_DMG_LVL_20, Weapon.WeaponType.STAFF));
+                        (STAFF, STAFF_WEIGHT, WEAPON_DMG_LVL_10, Weapon.WeaponType.STAFF));
             } else if (player.getLevel() > 20 && player.getLevel() <= 30) {
                 items.add(new Weapon
-                        (STAFF, STAFF_WEIGHT, WEAPON_DMG_LVL_30, Weapon.WeaponType.STAFF));
+                        (STAFF, STAFF_WEIGHT, WEAPON_DMG_LVL_20, Weapon.WeaponType.STAFF));
             } else if (player.getLevel() > 30 && player.getLevel() <= 40) {
                 items.add(new Weapon
-                        (STAFF, STAFF_WEIGHT, WEAPON_DMG_LVL_40, Weapon.WeaponType.STAFF));
+                        (STAFF, STAFF_WEIGHT, WEAPON_DMG_LVL_30, Weapon.WeaponType.STAFF));
             } else if (player.getLevel() > 40 && player.getLevel() <= 50) {
                 items.add(new Weapon
+                        (STAFF, STAFF_WEIGHT, WEAPON_DMG_LVL_40, Weapon.WeaponType.STAFF));
+            } else if (player.getLevel() > 50 && player.getLevel() < 60) {
+                items.add(new Weapon
                         (STAFF, STAFF_WEIGHT, WEAPON_DMG_LVL_50, Weapon.WeaponType.STAFF));
-            } else if (player.getLevel() > 50 && player.getLevel() <= 60) {
+            } else if (player.getLevel() == 60) {
                 items.add(new Weapon
                         (STAFF, STAFF_WEIGHT, WEAPON_DMG_LVL_60, Weapon.WeaponType.STAFF));
             }
@@ -85,12 +90,14 @@ public class Chest {
         }
     }
 
-    // TODO: This should probably be in the Player class.
+    // TODO: This method should be in the Player class.
     public ArrayList<Item> openChest(Player player) {
         generateItems(player);
         return getItems();
     }
 
+    // Todo: This method is public until openChest() gets implemented in Player.
+    //  Change to private when that happens.
     public ArrayList<Item> getItems() {
         return items;
     }
--- src/test/java/inte/grupp5/item/ChestTest.java
+++ src/test/java/inte/grupp5/item/ChestTest.java
@@ -1,22 +1,63 @@
 package inte.grupp5.item;
 
 import inte.grupp5.player.Player;
+import inte.grupp5.player.classes.Mage;
 import org.junit.jupiter.api.Test;
-
-import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-// TODO: Write test of a Chest class.
-//  It should take a number of Item objects and randomize based on Player level.
-//  Chest could be locked and only be opened by Player of same level or higher.
+// TODO: Since Player only has one Class (klass),
+//  only chests for Mages can be tested.
 public class ChestTest {
 
-    Player p1 = new Player();
+    Mage MAGE = new Mage("Gandalf");
+
+    Player LEVEL_5_MAGE = new Player("Level 5", MAGE, 5);
+    Player LEVEL_15_MAGE = new Player("Level 15", MAGE, 15);
+    Player LEVEL_25_MAGE = new Player("Level 25", MAGE, 25);
+    Player LEVEL_35_MAGE = new Player("Level 35", MAGE, 35);
+    Player LEVEL_45_MAGE = new Player("Level 45", MAGE, 45);
+    Player LEVEL_55_MAGE = new Player("Level 55", MAGE, 55);
+    Player LEVEL_60_MAGE = new Player("Level 60", MAGE, 60);
+
+    Chest LEVEL_5_MAGE_CHEST = new Chest();
+    Chest LEVEL_15_MAGE_CHEST = new Chest();
+    Chest LEVEL_25_MAGE_CHEST = new Chest();
+    Chest LEVEL_35_MAGE_CHEST = new Chest();
+    Chest LEVEL_45_MAGE_CHEST = new Chest();
+    Chest LEVEL_55_MAGE_CHEST = new Chest();
+    Chest LEVEL_60_MAGE_CHEST = new Chest();
 
     @Test
-    void chestGivesItems() {
-        //p1.openChest();
-        //assertEquals(items.get(0), p1.openChest());*/
+    void chestGeneratesWeaponWithDamageBasedOnLevelForMages() {
+        LEVEL_5_MAGE_CHEST.generateItems(LEVEL_5_MAGE);
+        assertEquals("[Name: Staff, Weight: 3.00, Damage: 5, Weapon type: STAFF]", LEVEL_5_MAGE_CHEST.getItems().get(3).toString());
+        // TODO: Workaround to check damage, since value is calculated by adding damage and weight.
+        assertEquals(8, LEVEL_5_MAGE_CHEST.getItems().get(3).getValue());
+
+
+        LEVEL_15_MAGE_CHEST.generateItems(LEVEL_15_MAGE);
+        assertEquals("[Name: Staff, Weight: 3.00, Damage: 10, Weapon type: STAFF]", LEVEL_15_MAGE_CHEST.getItems().get(3).toString());
+
+        LEVEL_25_MAGE_CHEST.generateItems(LEVEL_25_MAGE);
+        assertEquals("[Name: Staff, Weight: 3.00, Damage: 20, Weapon type: STAFF]", LEVEL_25_MAGE_CHEST.getItems().get(3).toString());
+
+        LEVEL_35_MAGE_CHEST.generateItems(LEVEL_35_MAGE);
+        assertEquals("[Name: Staff, Weight: 3.00, Damage: 30, Weapon type: STAFF]", LEVEL_35_MAGE_CHEST.getItems().get(3).toString());
+
+        LEVEL_45_MAGE_CHEST.generateItems(LEVEL_45_MAGE);
+        assertEquals("[Name: Staff, Weight: 3.00, Damage: 40, Weapon type: STAFF]", LEVEL_45_MAGE_CHEST.getItems().get(3).toString());
+
+        LEVEL_55_MAGE_CHEST.generateItems(LEVEL_55_MAGE);
+        assertEquals("[Name: Staff, Weight: 3.00, Damage: 50, Weapon type: STAFF]", LEVEL_55_MAGE_CHEST.getItems().get(3).toString());
+
+        LEVEL_60_MAGE_CHEST.generateItems(LEVEL_60_MAGE);
+        assertEquals("[Name: Staff, Weight: 3.00, Damage: 60, Weapon type: STAFF]", LEVEL_60_MAGE_CHEST.getItems().get(3).toString());
+    }
+
+    @Test
+    void generatedChestHas4ItemsForMages() {
+        LEVEL_25_MAGE_CHEST.generateItems(LEVEL_25_MAGE);
+        assertEquals(4, LEVEL_25_MAGE_CHEST.getItems().size());
     }
 }
