332b28eea9728a3bb337258cdf6e9c287fd5f200
Jonathan <joas4713@student.dsv.se>
2020-10-29 15:34:46 +0100

    Terrain package removed.

--- src/main/java/inte/grupp5/terrain/Terrain.java
+++
@@ -1,4 +0,0 @@
-package inte.grupp5.terrain;
-
-public class Terrain {
-}
--- src/test/java/inte/grupp5/terrain/TerrainTest.java
+++
@@ -1,9 +0,0 @@
-package inte.grupp5.terrain;
-
-import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.*;
-
-class TerrainTest {
-
-}
