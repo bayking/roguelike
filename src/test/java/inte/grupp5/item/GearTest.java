0190257fbbe1aa0320be8410d211e201faee4305
Jonathan <joas4713@student.dsv.se>
2021-09-10 15:17:00 +0200

    Full coverage test for Gear

--- src/test/java/inte/grupp5/item/GearTest.java
+++ src/test/java/inte/grupp5/item/GearTest.java
@@ -1,5 +1,6 @@
 package inte.grupp5.item;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -25,6 +26,36 @@ class GearTest {
         assertEquals(Gear.Enchantment.QUAD_DAMAGE, GLOVES_WITH_QUAD.getEnchantment());
         assertEquals(Gear.Enchantment.INVISIBILITY, GLOVES_WITH_INVIS.getEnchantment());
         assertEquals(Gear.Enchantment.PROTECTION, GLOVES_WITH_PROTEC.getEnchantment());
+    }
+
+    @Test
+    void gearEnchantmentHasCorrectDuration() {
+        assertEquals(0, GLOVES_WITH_NO_ENCHANTMENT.getEnchantment().getDuration());
+        assertEquals(15, GLOVES_WITH_QUAD.getEnchantment().getDuration());
+        assertEquals(30, GLOVES_WITH_INVIS.getEnchantment().getDuration());
+        assertEquals(20, GLOVES_WITH_PROTEC.getEnchantment().getDuration());
+    }
+
+    @Test
+    void gearEnchantmentHasCorrectDamageModifier() {
+        assertEquals(1, GLOVES_WITH_NO_ENCHANTMENT.getEnchantment().getDamageModifier());
+        assertEquals(4, GLOVES_WITH_QUAD.getEnchantment().getDamageModifier());
+        assertEquals(1, GLOVES_WITH_INVIS.getEnchantment().getDamageModifier());
+        assertEquals(1, GLOVES_WITH_PROTEC.getEnchantment().getDamageModifier());
+    }
+
+    @Test
+    void gearEnchantmentHasCorrectArmorModifier() {
+        assertEquals(1, GLOVES_WITH_NO_ENCHANTMENT.getEnchantment().getArmorModifier());
+        assertEquals(1, GLOVES_WITH_QUAD.getEnchantment().getArmorModifier());
+        assertEquals(1, GLOVES_WITH_INVIS.getEnchantment().getArmorModifier());
+        assertEquals(10, GLOVES_WITH_PROTEC.getEnchantment().getArmorModifier());
+    }
+
+    @Test
+    void armorWithNegativeArmorRatingThrowsIAE() {
+        Assertions.assertThrows(IllegalArgumentException.class, () ->
+                new Armor("Gear", 1, -1, Armor.ArmorType.LIGHT_ARMOR, Gear.Enchantment.NONE));
     }
 
 }
